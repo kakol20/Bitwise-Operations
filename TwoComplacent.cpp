ac592f590a77f07c4720fcf95bb1d2a18966b050
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2021-11-11 22:30:47 +0000

    Format TwoComplacent.cpp

--- TwoComplacent.cpp
+++ TwoComplacent.cpp
@@ -1,17 +1,14 @@
 #include "TwoComplacent.h"
 
-TwoComplacent::TwoComplacent()
-{
+TwoComplacent::TwoComplacent() {
 }
 
-int8_t TwoComplacent::OneToTwo(const uint8_t n)
-{
+int8_t TwoComplacent::OneToTwo(const uint8_t n) {
 	// flip and + 1
 	return ~n + 1;
 }
 
-uint8_t TwoComplacent::TwoToOne(const int8_t n)
-{
+uint8_t TwoComplacent::TwoToOne(const int8_t n) {
 	return ~(n - 1);
 }
 
